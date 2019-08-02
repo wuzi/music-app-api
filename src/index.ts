938b9116ee90c6d878493d0171754a0ace6671dd
Lucas Godoy <lcsdegodoy@outlook.com>
2019-08-02 20:16:57 -0300

    chore(env): load database connection string from env vars

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import Koa from 'koa';
 import koaSwagger from 'koa2-swagger-ui';
 import bodyParser from 'koa-bodyparser';
+import dotenv from 'dotenv';
 import routeLoader from './routes';
+import mongoose from 'mongoose';
+dotenv.config();
 
 const app = new Koa();
 const port = process.env.PORT || 3000;
 
-import mongoose from 'mongoose';
-mongoose.connect('mongodb://localhost/music', { useNewUrlParser: true });
+mongoose.connect(<string>process.env.DATABASE, { useNewUrlParser: true }).then(() => {
+  console.log(`Database connected successfully.`);
+}).catch(() => {
+  console.log(`ERROR: Could not connnect to database.`);
+});
 
 app.use(bodyParser());
 app.use(koaSwagger({ routePrefix: '/docs', swaggerOptions: { url: `http://localhost:${port}/swagger.json` } }));
