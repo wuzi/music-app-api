e9efbc34d7b0e9d1456153510cd2bdc896d1ae0d
Lucas Godoy <lcsdegodoy@outlook.com>
2019-08-07 13:56:34 -0300

    chore(eslint): add eslint

--- src/index.ts
+++ src/index.ts
@@ -12,16 +12,16 @@ dotenv.config();
 const app = new Koa();
 const port = process.env.PORT || 3000;
 
-mongoose.connect(<string>process.env.DATABASE, { useNewUrlParser: true }).then(() => {
+mongoose.connect(process.env.DATABASE as string, { useNewUrlParser: true }).then((): void => {
   logger('database', 'Database connected successfully.', 'info');
-}).catch(() => {
+}).catch((): void => {
   logger('database', 'Could not connnect to database.', 'error');
 });
 
 app.use(cors());
 app.use(bodyParser());
-app.use(koaSwagger({ routePrefix: '/docs', swaggerOptions: { url: `/swagger.json` } }));
-app.use(jwt({ secret: <string>process.env.SECRET }).unless({ path: [/^\/swagger.json/, /^\/docs/, /\/$/, /^\/v1\/login/, /^\/v1\/register/] }));
+app.use(koaSwagger({ routePrefix: '/docs', swaggerOptions: { url: '/swagger.json' } }));
+app.use(jwt({ secret: process.env.SECRET as string }).unless({ path: [/^\/swagger.json/, /^\/docs/, /\/$/, /^\/v1\/login/, /^\/v1\/register/] }));
 routeLoader(app);
 
-app.listen(port, () => logger('index', `Server started at http://localhost:${port}`, 'info'));
+app.listen(port, (): void => logger('index', `Server started at http://localhost:${port}`, 'info'));
